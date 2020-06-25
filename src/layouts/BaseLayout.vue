074b21fa9392ccd7dd4e2b4c27eab23c440ac3ba
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-06-25 00:38:17 -0500

    primera clase

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar dense dark flat app>
+      <!-- -->
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+
+    <v-bottom-navigation v-model="bottomNav" v-if="isMobile" app>
+      <v-btn value="recent">
+        <span>Recent</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bottomNav: 'favorites',
+      isMobile: false
+    }
+  },
+  beforeDestroy () {
+    if (typeof window != 'undefined') {
+      window.removeEventListener('resize', this.onResize, { passive: true })
+    }
+  },
+  mounted () {
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+  methods: {
+    onResize () {
+      this.isMobile = window.innerWidth < 680
+    }
+  }
+}
+</script>
